9111c4f7628060260e40b207d28afee7582995d5
v20035017 <camille.vedani@outlook.fr>
2023-02-16 14:47:25 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is ahead of 'origin/master' by 4 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   InsertionSort.py
    	new file:   __pycache__/ArrayFunc.cpython-38.pyc

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,10 +1,15 @@
+import ArrayFunc
+
 def InsertionSort(Array) :
+    print(Array)
     for k in range (1,len(Array)):
         key = Array[k]
         i = k-1
-        while i>0 and Array[i]>key :
+        while i>=0 and Array[i]>key :
             Array[i+1] = Array[i]
             i = i-1
             Array[i+1] = key
     return(Array)
     
+
+print(InsertionSort(ArrayFunc.GenerateRandomArray(6,-15,15)))
